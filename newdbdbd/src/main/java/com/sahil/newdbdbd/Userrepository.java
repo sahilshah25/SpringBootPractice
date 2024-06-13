dfaa948b7558349cfa1d218bec7fe3f608c400d9
sahilshah25 <sahilshah84621@gmail.com>
2024-06-13 11:36:46 +0530

    Delete newdbdbd directory

--- newdbdbd/src/main/java/com/sahil/newdbdbd/Userrepository.java
+++
@@ -1,7 +0,0 @@
-package com.sahil.newdbdbd;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface Userrepository extends MongoRepository<ModelUser,String> {
-}
